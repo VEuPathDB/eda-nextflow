769213c58f47bec9f9bb9062f181bce96d6861fc
John Brestelli <jbrestell@gmail.com>
2023-07-19 13:57:49 -0400

    optionalCollectionsYaml

--- modules/templates/loadDatasetSpecificTables.bash
+++ modules/templates/loadDatasetSpecificTables.bash
@@ -2,7 +2,12 @@
 
 set -euo pipefail
 
-ga ApiCommonData::Load::Plugin::LoadDatasetSpecificEntityGraph \\
+if [ "$params.optionalCollectionsYaml" != "NA" ] ; then
+  internalCollectionsYaml="--collectionsYaml $params.optionalCollectionsYaml";
+fi
+
+
+ga ApiCommonData::Load::Plugin::LoadDatasetSpecificEntityGraph \$internalCollectionsYaml \\
     --extDbRlsSpec \'$extDbRlsSpec\' \\
     --schema $params.schema \\
     --commit;
